b1ea3f927b23a67d853de62e7e459ad1126444e6
Wicked <bekarov@gmail.com>
2022-06-12 13:19:44 +0200

    Initial commit

---
+++ src/HomeworkGit.java
@@ -0,0 +1,13 @@
+public class HomeworkGit {
+    public static void main(String[] args) {
+        hello("Tome");
+        goodbye("Tome");
+    }
+
+    public static void hello(String name){
+        System.out.println("Hello " + name);
+    }
+    public static void goodbye(String name){
+        System.out.println("Goodbye " + name);
+    }
+}
